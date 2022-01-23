6f3da96fbe3e3dc62757ea98fd8cc7a14f21f73e
Kenny You <kenny_you@Kennys-MBP-2.lan>
2022-01-22 19:22:21 -0800

    First commit for Chatter App.

---
+++ chatter/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo"></div>
+        <span className="title">ChitChat</span>
+      </header>
+      <footer className="footer">
+        <input className="text-input"></input>
+        <button className="send">👆</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
