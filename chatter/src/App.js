65c240433207e109105572d490400622b795211c
Kenny You <kenny_you@Kennys-MBP-2.lan>
2022-01-28 22:21:02 -0800

    Exercise 4.1: added logo spin and other styling for app.

--- chatter/src/App.js
+++ chatter/src/App.js
@@ -8,6 +8,11 @@ function App() {
         <div className="logo"></div>
         <span className="title">ChitChat</span>
       </header>
+      <body>
+        <div class = "messages">
+          <p1>Can I get a waffle? 🧇</p1>
+        </div>
+      </body>
       <footer className="footer">
         <input className="text-input"></input>
         <button className="send">👆</button>
